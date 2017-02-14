4d06d55fe0d1e2eeba76e12f32b16973e830e7ca
John Carlson <jawnnypoo@gmail.com>
2017-02-14 16:23:52 -0600

    Sample!

--- app/src/main/java/com/commit451/swipedismisstouchlistener/sample/MainActivity.java
+++ app/src/main/java/com/commit451/swipedismisstouchlistener/sample/MainActivity.java
@@ -1,7 +1,13 @@
 package com.commit451.swipedismisstouchlistener.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.commit451.swipedismisstouchlistener.SwipeDismissTouchListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ViewGroup root = (ViewGroup) findViewById(R.id.root);
+
+        View view = findViewById(R.id.card1);
+        SwipeDismissTouchListener swipeDismissTouchListener = new SwipeDismissTouchListener(view);
+        swipeDismissTouchListener.setOnDismissListener(new SwipeDismissTouchListener.OnDismissListener() {
+            @Override
+            public void onDismiss(@NonNull View view, @Nullable Object token) {
+                root.removeView(view);
+            }
+        });
+        view.setOnTouchListener(swipeDismissTouchListener);
 
     }
 }
